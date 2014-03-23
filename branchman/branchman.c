149fbc8850e45c336af43aa70cd7bec1872fb3f6
Morketh <admmorketh@gmail.com>
2014-03-22 21:14:29 -0700

    ADD: git checkout feature development

--- branchman/branchman.c
+++ branchman/branchman.c
@@ -2,10 +2,22 @@
 #include <stdio.h>
 #include "./lib/logo.h"
 
+char line[256];
 int main ( void )
 {
+  
   system ( "clear" ); /* Or system ( "clear" ); for POSIX */
-  printf ("screen cleared\n");
+  logo();
+  printf("new Feature: ");
+  if(fgets(line, sizeof line, stdin) != NULL)
+  {
+	system("git checkout -b "+line+" development")
+  /* Now inspect and further parse the string in line. */
+  }
+  else 
+  {
+	printf("error");
+  }
   logo();
   return 0;
 }
